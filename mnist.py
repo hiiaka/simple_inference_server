f3617ffff4ab5c07e96c03029a2ea71adee6c5cc
Shuya Saeki <saeki_s@araya.org>
2023-06-01 15:19:41 +0900

    Added ClearML package

--- mnist.py
+++ mnist.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist
 
+from clearml import Task
+task = Task.init(project_name='MNIST', task_name='mnist training')
+
 # Load and preprocess the MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.astype("float32") / 255.0
@@ -12,16 +15,18 @@ y_train = tf.keras.utils.to_categorical(y_train, 10)
 y_test = tf.keras.utils.to_categorical(y_test, 10)
 
 # Build the model
-model = tf.keras.Sequential([
-    layers.InputLayer(input_shape=(28, 28, 1)),
-    layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-    layers.MaxPooling2D(pool_size=(2, 2)),
-    layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-    layers.MaxPooling2D(pool_size=(2, 2)),
-    layers.Flatten(),
-    layers.Dropout(0.5),
-    layers.Dense(10, activation="softmax")
-])
+model = tf.keras.Sequential(
+    [
+        layers.InputLayer(input_shape=(28, 28, 1)),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(10, activation="softmax"),
+    ]
+)
 
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
